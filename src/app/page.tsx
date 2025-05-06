7e7b2bce14e9579e5f950ae8dab5e3231029333a
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-06 11:11:14 -0300

    feat: ✨ add prettier and eslint configurations with new images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Restaurant } from "@/types/restaurant"
 export default function Home() {
   return (
     <main className="p-4 space-y-2">
-      <h1 className="text-xl font-bold mb-2">abertos</h1>
+      <h1 className="text-xl font-bold mb-2 text-purple-text">abertos</h1>
       <div className="space-y-2">
         {(restaurants as Restaurant[]).map((r) => (
           <RestaurantCard key={r.id} {...r} />
