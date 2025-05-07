30db07f82b85365336f4c95abf93b6c267befc7f
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-05-07 17:31:39 -0300

    feat: ✨ add loading component for improved user experience

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-12 h-12 border-4 border-t-transparent border-purple-text rounded-full animate-spin"></div>
+    </div>
+  );
+}
